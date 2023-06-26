a071bf418ab74681d13a47e258b841fef656bf2e
Daniel-Montana <danielmontana2303@gmail.com>
2023-06-26 10:07:53 -0500

    a

---
+++ Datos_2.R
@@ -0,0 +1,7 @@
+###################### LEER LAS BASES DE DATOS #################################
+base<- "C:\\Users\\karme\\Desktop\\Prácticas\\Datos\\Encuesta Multiproposito\\Microdatos\\Multiproposito_completa.rds"
+EM21F <- readRDS(base)
+
+library(dplyr)
+
+
